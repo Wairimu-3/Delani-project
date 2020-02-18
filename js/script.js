ae29aff4d11390aafe67383bdf59786b45afc131
Margaret Wairimu Kimani-1040 <kimanimargaret46@gmail.com>
2020-02-18 12:46:40 +0300

    validated submit button

--- js/script.js
+++ js/script.js
@@ -14,7 +14,21 @@ $(document).ready(function() {
     });
 });
 
-   
+             //PORTFOLIO HOVER
+$(document).ready(function() {
+    $("div.image-frame").hover(function() {
+        $(".classAdd1").addClass("mask flex-center rgba-red-strong");
+    });
+    $("div.image-frame").hover(function() {
+        $(".classAdd2").addClass("mask flex-center rgba-blue-light");
+    });
+    $("div.image-frame").hover(function() {
+        $(".classAdd3").addClass("mask flex-center rgba-green-slight");
+    });
+    $("div.image-frame").hover(function() {
+        $(".classAdd4").addClass("mask flex-center");
+    });
+});
 
 var btn = document.getElementById("myBtn");
                       // OPEN MODAL ON BUTTON CLICK
